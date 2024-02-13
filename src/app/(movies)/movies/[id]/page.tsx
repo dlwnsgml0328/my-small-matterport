c165f3bcb6df8f48bab5aa45326f4b6d3359b644
junhee <junhee@eazel.net>
2024-02-13 12:18:16 +0900

    ♻️ (refactor): Improve the performance for fetching data
    Promise all을 사용하여 병렬적으로 데이터를 패칭할 수 있게 변경

--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const fetchMovie = async (id: string) => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   console.log(`Fetching movies: ${Date.now()}`);
   const response = await fetch(`https://nomad-movies.nomadcoders.workers.dev/movies/${id}`);
   const movie = await response.json();
@@ -18,8 +18,9 @@ const getVidoes = async (id: string) => {
 
 const Movie = async ({ params: { id } }: { params: { id: string } }) => {
   console.log('start fetching');
-  const movie = await fetchMovie(id);
-  const videos = await getVidoes(id);
+
+  const [movie, videos] = await Promise.all([fetchMovie(id), getVidoes(id)]);
+
   console.log('end fetching');
 
   return (
